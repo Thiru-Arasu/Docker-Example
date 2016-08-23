e7b725b4d376904e7b3b21009417b23965ec6631
thirunavukkarasumca <thiru@boomerangcommerce.com>
2016-08-22 22:14:07 -0700

    Added supports for Nginx and Python

---
+++ Nginx-Python-config/app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<H1> Hello World!!! </H1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
